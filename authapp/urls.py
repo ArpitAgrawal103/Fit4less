5c73c757cac7d63a9a68f44bd80d4e136a87f59b
ArpitAgrawal103 <arpitagrawal103@gmail.com>
2024-07-06 13:36:32 +0530

    Initial commit

---
+++ authapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from authapp import views
+
+urlpatterns = [
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handlelogout,name="handlelogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+]
